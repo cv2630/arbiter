45f2ff401c9b713b7049622b508e03ec593a21ba
Vincent Chen <chen.vincent.ny@gmail.com>
2026-08-22 21:21:58 -0400

    added finding best move, working on negamax algorithm

--- src/search.cpp
+++ src/search.cpp
@@ -3,3 +3,47 @@
 #include <chrono>
 #include <iostream>
 #include <limits>
+
+using namespace chess;
+
+namespace {
+
+constexpr int infty=99999999; //
+constexpr int checkmate=999999;
+
+int negamax(Board& board, int depth) {
+//most sources use minimax, switch to negamax for single maximization?
+
+}
+
+} // namespace
+
+Move findBestMove(Board& board, const SearchLimits& limits) {
+    Movelist moves;
+    movegen::legalmoves(moves, board);
+    if(moves.empty()) {
+        return Move::NO_MOVE;
+    }
+
+    int depth;
+    if(limits.depth > 0) {
+        depth=limits.depth; } 
+    else {
+        depth=4;
+    }
+    Move bestMove= moves[0];
+    int bestScore= -infty;
+
+
+    for(const auto&m:moves) {
+
+        board.makeMove(m);
+        int score= -negamax(board, depth - 1);
+        board.unmakeMove(m);
+        if (score > bestScore) {
+            bestScore=score;
+            bestMove= m;
+        }}
+    return bestMove;
+
+}
