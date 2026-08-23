88a856d0d408b1d737ec1229e7c924cb17d2667d
Vincent Chen <chen.vincent.ny@gmail.com>
2026-08-23 00:02:18 -0400

    added changes to uciloop based on google search from disservin library, mvp nearly complete, working on terminal interface

--- src/uci.cpp
+++ src/uci.cpp
@@ -1,12 +1,12 @@
 #include "uci.h"
 #include "search.h"
 #include "chess.hpp"
+#include <iostream>
+#include <sstream>
+#include <vector>
 using namespace chess;
 
 namespace {
-
-//add
-
     std::string pieceSymbol(Piece p) {
     switch (p.internal()) {
         case Piece::WHITEPAWN:   return "♙";
@@ -30,9 +30,6 @@ void printBoard(const Board& board) {
     disservin writes pieces w following idx
     index = file + 8 * rank
     */
-
-    //google ```echo -e "\e[30;43m This text has a yellow background \e[0m"```
-
     std::string black = "\e[37m\e[40m";
     std::string white = "\e[37m\e[47m";
     std::string reset= "\e[0m";
@@ -57,14 +54,75 @@ for(int row = 0; row < 8; row++) {
 
 
 }
+/*
+note: the uci command parsing in uci.cpp follows the standard 
+token-based approach for this protocol (istringstream + dispatch on first word)
+*/
 
 void uciLoop() {
+    chess::Board board;
+    std::string line;
+
+    while (std::getline(std::cin, line)) {
+        std::istringstream stream(line);
+        std::string token;
+        stream >> token;
+
+        if (token == "uci") {
+            std::cout << "id name SimpleChessEngine\n";
+            std::cout << "id author Me\n";
+            std::cout << "uciok\n";
+        } 
+        else if (token == "isready") {
+            std::cout << "readyok\n";
+        } 
+        else if (token == "position") {
+            std::string subtoken;
+            stream >> subtoken;
+            if (subtoken == "startpos") {
+                board.setFen(constants::STARTPOS);
+                if (stream >> subtoken && subtoken == "moves") {
+                    std::string moveStr;
+                    while (stream >> moveStr) {
+                        chess::Move move = chess::uci::uciToMove(board, moveStr);
+                        board.makeMove(move);
+                    }
+                }
+            } else if (subtoken == "fen") {
+                std::string fenParts;
+                // Accumulate FEN string (6 fields)
+                std::string fen = "";
+                for (int i = 0; i < 6; ++i) {
+                    stream >> subtoken;
+                    fen += subtoken + (i < 5 ? " " : "");
+                }
+                board.setFen(fen);
+                if (stream >> subtoken && subtoken == "moves") {
+                    std::string moveStr;
+                    while (stream >> moveStr) {
+                        chess::Move move = chess::uci::uciToMove(board, moveStr);
+                        board.makeMove(move);
+                    }
+                }
+            }
+        } 
+        else if (token == "go") {
+            SearchLimits limits;
+            std::string sub;
+            while (stream >> sub) {
+                if (sub == "depth") stream >> limits.depth;
+                else if (sub == "movetime") stream >> limits.movetime;
+                else if (sub == "wtime") stream >> limits.wtime;
+                else if (sub == "btime") stream >> limits.btime;
+                else if (sub == "winc") stream >> limits.winc;
+                else if (sub == "binc") stream >> limits.binc;
+                else if (sub == "infinite") limits.infinite = true;
+            }
+            Move best = findBestMove(board, limits);
+            std::cout << "bestmove " << uci::moveToUci(best) << "\n";
+        }
+        else if (token == "quit") {
+            break;
+        }
+    }
 }
-
-/*int main()
-{
-    std::cout<<"Hello World";
-    printBoard();
-
-    return 0;
-*/
