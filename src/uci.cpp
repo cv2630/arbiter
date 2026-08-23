66e541d0da8db5f9af53f04a3ec3c47d2ab524d4
Vincent Chen <chen.vincent.ny@gmail.com>
2026-08-22 21:49:03 -0400

    found pieces online for black/white, added

--- src/uci.cpp
+++ src/uci.cpp
@@ -3,7 +3,37 @@
 #include "chess.hpp"
 using namespace chess;
 
-namespace { }
+namespace {
+
+//add
+
+    std::string pieceSymbol(Piece p) {
+    switch (p.internal()) {
+        case Piece::WHITEPAWN:   return "♙";
+        case Piece::WHITEKNIGHT: return "♘";
+        case Piece::WHITEBISHOP: return "♗";
+        case Piece::WHITEROOK:   return "♖";
+        case Piece::WHITEQUEEN:  return "♕";
+        case Piece::WHITEKING:   return "♔";
+        case Piece::BLACKPAWN:   return "♟";
+        case Piece::BLACKKNIGHT: return "♞";
+        case Piece::BLACKBISHOP: return "♝";
+        case Piece::BLACKROOK:   return "♜";
+        case Piece::BLACKQUEEN:  return "♛";
+        case Piece::BLACKKING:   return "♚";
+        default:                 return " ";
+    }
+}
+
+void printBoard(const Board& board) {
+    std::string black = "\e[37m\e[40m";
+    std::string white = "\e[37m\e[47m";
+    std::string reset= "\033[0m";
+
+}
+
+
+}
 
 void uciLoop() {
 }
